d0a97cfbbba7de8cbe3562444b90a2c367b01314
Prakriti Kesharwani <pkpk6246@gmail.com>
2023-06-01 02:25:22 +0530

    Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors: {
         lightGray: "hsl(212, 45%, 89%)",
         grayishBlue: "hsl(220, 15%, 55%)",
-        darkBlue: "hsl(218, 44%, 22%)",
+        white: "hsl(0, 0%, 100%)"
       },
     },
   },
