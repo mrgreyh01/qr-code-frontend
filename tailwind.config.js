5cbeed52b803a57b82fe87d693804b1ec243d8ba
Prakriti Kesharwani <pkpk6246@gmail.com>
2023-05-31 14:14:24 +0530

    Add Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./images/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
   },
   plugins: [],
 }
